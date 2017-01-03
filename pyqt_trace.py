02b57c276613854615ebf83c88a35f3df1418e32
Per Gröningsson <per.groningsson@vattenfall.com>
2017-01-03 15:03:45 +0100

    imported progbar.py and pyqt_trace.py

---
+++ pyqt_trace.py
@@ -0,0 +1,13 @@
+def pyqt_trace():
+    '''Set a tracepoint in the Python debugger that works with Qt'''
+    from PyQt4.QtCore import pyqtRemoveInputHook
+    import pdb
+    import sys
+    pyqtRemoveInputHook()
+    # set up the debugger
+    debugger = pdb.Pdb()
+    debugger.reset()
+    # custom next to get outside of function scope
+    debugger.do_next(None) # run the next command
+    users_frame = sys._getframe().f_back # frame where the user invoked `pyqt_set_trace()`
+    debugger.interaction(users_frame, None)
