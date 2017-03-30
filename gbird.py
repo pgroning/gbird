dcf0de59aee1bbc72a9cd816146246408304bc07
Per Gröningsson <per.groningsson@vattenfall.com>
2017-03-30 12:50:02 +0200

    moved EnrDialog to pin module

--- gbird.py
+++ gbird.py
@@ -40,7 +40,7 @@ from plot import PlotWin
 from dlg_cascalc import CasDialog, CasRunDialog
 from dlg_bundle import BundleDialog
 from dlg_report import ReportDialog
-from pin import FuePin
+from pin import FuePin, EnrDialog
 from progbar import ProgressBar
 from map_s96 import s96o2
 from map_a10 import a10xm
@@ -144,70 +144,70 @@ class dataThread(QtCore.QThread):
 #            return False
 #"""
 
-class EnrDialog(QtGui.QDialog):
-    def __init__(self, parent, mode="edit"):
-        # QDialog.__init__(self, parent)
-        QtGui.QDialog.__init__(self)
-        self.parent = parent
-        self.mode = mode
-
-        # self.setWindowTitle("Window title")
-        # set x-pos relative to cursor position
-        # xpos = QCursor.pos().x() - 250
-        # set dialog pos relative to main window
-        xpos = parent.pos().x() + parent.size().width() / 2
-        ypos = parent.pos().y() + parent.size().height() / 2
-        self.setGeometry(QtCore.QRect(xpos, ypos, 150, 120))
-
-        case_num = int(parent.case_cbox.currentIndex())
-        ipin = parent.pinselection_index
-        enr = parent.enrpinlist[case_num][ipin].ENR
-        ba = parent.enrpinlist[case_num][ipin].BA
-        ba = 0 if np.isnan(ba) else ba
-        if mode == "edit":
-            self.setWindowTitle("Edit enrichment")
-        elif mode == "add":
-            self.setWindowTitle("Add enrichment")
-        self.enr_text = QtGui.QLineEdit("%.2f" % enr)
-        # dens = parent.enrpinlist[case_num][ipin].DENS
-        # self.dens_text = QtGui.QLineEdit("%.3f" % dens)
-        self.ba_text = QtGui.QLineEdit("%.2f" % ba)
-        validator = QtGui.QDoubleValidator(0, 9.99, 2, self)
-        self.enr_text.setValidator(validator)
-        self.ba_text.setValidator(validator)
-        # validator = QtGui.QDoubleValidator(0, 9.99, 3, self)
-        # self.dens_text.setValidator(validator)
-
-        flo = QtGui.QFormLayout()
-        flo.addRow("%U-235:", self.enr_text)
-        #flo.addRow("Density (g/cm-3):", self.dens_text)
-        flo.addRow("%Gd:", self.ba_text)
-
-        hbox = QtGui.QHBoxLayout()
-        self.ok_button = QtGui.QPushButton("Ok")
-        self.cancel_button = QtGui.QPushButton("Cancel")
-        hbox.addWidget(self.cancel_button)
-        hbox.addWidget(self.ok_button)
-        self.connect(self.cancel_button, QtCore.SIGNAL('clicked()'),
-                     self.close)
-        self.connect(self.ok_button, QtCore.SIGNAL('clicked()'), self.action)
-
-        vbox = QtGui.QVBoxLayout()
-        vbox.addLayout(flo)
-        vbox.addStretch()
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
-
-    def action(self):
-        self.close()
-        self.enr = self.enr_text.text().toDouble()[0]
-        #self.dens = self.dens_text.text().toDouble()[0]
-        self.ba = self.ba_text.text().toDouble()[0]
-        if self.mode == "edit":
-            self.parent.enrpin_edit_callback()
-        elif self.mode == "add":
-            self.parent.enrpin_add_callback()
-
+#class EnrDialog(QtGui.QDialog):
+#    def __init__(self, parent, mode="edit"):
+#        # QDialog.__init__(self, parent)
+#        QtGui.QDialog.__init__(self)
+#        self.parent = parent
+#        self.mode = mode
+#
+#        # self.setWindowTitle("Window title")
+#        # set x-pos relative to cursor position
+#        # xpos = QCursor.pos().x() - 250
+#        # set dialog pos relative to main window
+#        xpos = parent.pos().x() + parent.size().width() / 2
+#        ypos = parent.pos().y() + parent.size().height() / 2
+#        self.setGeometry(QtCore.QRect(xpos, ypos, 150, 120))
+#
+#        case_num = int(parent.case_cbox.currentIndex())
+#        ipin = parent.pinselection_index
+#        enr = parent.enrpinlist[case_num][ipin].ENR
+#        ba = parent.enrpinlist[case_num][ipin].BA
+#        ba = 0 if np.isnan(ba) else ba
+#        if mode == "edit":
+#            self.setWindowTitle("Edit enrichment")
+#        elif mode == "add":
+#            self.setWindowTitle("Add enrichment")
+#        self.enr_text = QtGui.QLineEdit("%.2f" % enr)
+#        # dens = parent.enrpinlist[case_num][ipin].DENS
+#        # self.dens_text = QtGui.QLineEdit("%.3f" % dens)
+#        self.ba_text = QtGui.QLineEdit("%.2f" % ba)
+#        validator = QtGui.QDoubleValidator(0, 9.99, 2, self)
+#        self.enr_text.setValidator(validator)
+#        self.ba_text.setValidator(validator)
+#        # validator = QtGui.QDoubleValidator(0, 9.99, 3, self)
+#        # self.dens_text.setValidator(validator)
+#
+#        flo = QtGui.QFormLayout()
+#        flo.addRow("%U-235:", self.enr_text)
+#        #flo.addRow("Density (g/cm-3):", self.dens_text)
+#        flo.addRow("%Gd:", self.ba_text)
+#
+#        hbox = QtGui.QHBoxLayout()
+#        self.ok_button = QtGui.QPushButton("Ok")
+#        self.cancel_button = QtGui.QPushButton("Cancel")
+#        hbox.addWidget(self.cancel_button)
+#        hbox.addWidget(self.ok_button)
+#        self.connect(self.cancel_button, QtCore.SIGNAL('clicked()'),
+#                     self.close)
+#        self.connect(self.ok_button, QtCore.SIGNAL('clicked()'), self.action)
+#
+#        vbox = QtGui.QVBoxLayout()
+#        vbox.addLayout(flo)
+#        vbox.addStretch()
+#        vbox.addLayout(hbox)
+#        self.setLayout(vbox)
+#
+#    def action(self):
+#        self.close()
+#        self.enr = self.enr_text.text().toDouble()[0]
+#        #self.dens = self.dens_text.text().toDouble()[0]
+#        self.ba = self.ba_text.text().toDouble()[0]
+#        if self.mode == "edit":
+#            self.parent.enrpin_edit_callback()
+#        elif self.mode == "add":
+#            self.parent.enrpin_add_callback()
+#"""
 
 class MainWin(QtGui.QMainWindow):
     def __init__(self, parent=None):
--- pin.py
+++ pin.py
@@ -1,6 +1,7 @@
 from pyqt_trace import pyqt_trace as qtrace  # Break point that works with Qt
 
 from PyQt4 import QtGui, QtCore
+import numpy as np
 
 import matplotlib.patches as mpatches
 try:  # patheffects not available for older versions of matplotlib
@@ -49,3 +50,68 @@ class FuePin(object):
             return True
         else:
             return False
+
+
+class EnrDialog(QtGui.QDialog):
+    def __init__(self, parent, mode="edit"):
+        # QDialog.__init__(self, parent)
+        QtGui.QDialog.__init__(self)
+        self.parent = parent
+        self.mode = mode
+
+        # self.setWindowTitle("Window title")
+        # set x-pos relative to cursor position
+        # xpos = QCursor.pos().x() - 250
+        # set dialog pos relative to main window
+        xpos = parent.pos().x() + parent.size().width() / 2
+        ypos = parent.pos().y() + parent.size().height() / 2
+        self.setGeometry(QtCore.QRect(xpos, ypos, 150, 120))
+
+        case_num = int(parent.case_cbox.currentIndex())
+        ipin = parent.pinselection_index
+        enr = parent.enrpinlist[case_num][ipin].ENR
+        ba = parent.enrpinlist[case_num][ipin].BA
+        ba = 0 if np.isnan(ba) else ba
+        if mode == "edit":
+            self.setWindowTitle("Edit enrichment")
+        elif mode == "add":
+            self.setWindowTitle("Add enrichment")
+        self.enr_text = QtGui.QLineEdit("%.2f" % enr)
+        # dens = parent.enrpinlist[case_num][ipin].DENS
+        # self.dens_text = QtGui.QLineEdit("%.3f" % dens)
+        self.ba_text = QtGui.QLineEdit("%.2f" % ba)
+        validator = QtGui.QDoubleValidator(0, 9.99, 2, self)
+        self.enr_text.setValidator(validator)
+        self.ba_text.setValidator(validator)
+        # validator = QtGui.QDoubleValidator(0, 9.99, 3, self)
+        # self.dens_text.setValidator(validator)
+
+        flo = QtGui.QFormLayout()
+        flo.addRow("%U-235:", self.enr_text)
+        #flo.addRow("Density (g/cm-3):", self.dens_text)
+        flo.addRow("%Gd:", self.ba_text)
+
+        hbox = QtGui.QHBoxLayout()
+        self.ok_button = QtGui.QPushButton("Ok")
+        self.cancel_button = QtGui.QPushButton("Cancel")
+        hbox.addWidget(self.cancel_button)
+        hbox.addWidget(self.ok_button)
+        self.connect(self.cancel_button, QtCore.SIGNAL('clicked()'),
+                     self.close)
+        self.connect(self.ok_button, QtCore.SIGNAL('clicked()'), self.action)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addLayout(flo)
+        vbox.addStretch()
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+    def action(self):
+        self.close()
+        self.enr = self.enr_text.text().toDouble()[0]
+        #self.dens = self.dens_text.text().toDouble()[0]
+        self.ba = self.ba_text.text().toDouble()[0]
+        if self.mode == "edit":
+            self.parent.enrpin_edit_callback()
+        elif self.mode == "add":
+            self.parent.enrpin_add_callback()
