b3d2b9d412d5aa12d13d91091f795bea3631a56f
Per Gröningsson <per.groningsson@gmail.com>
2017-04-05 22:57:42 +0200

    added qtableview to dlg_egv

--- dlg_egv.py
+++ dlg_egv.py
@@ -10,8 +10,19 @@ class EgvDialog(QtGui.QDialog):
         self.setWindowTitle("EGV settings")
         xpos = self.parent.pos().x() + self.parent.size().width() / 2
         ypos = self.parent.pos().y() + self.parent.size().height() / 2
-        self.setGeometry(QtCore.QRect(0.8*xpos, 0.9*ypos, 150, 120))
+        self.setGeometry(QtCore.QRect(0.8*xpos, 0.9*ypos, 500, 200))
 
+        self.table_view = QtGui.QTableView()
+        model = QtGui.QStandardItemModel(0, 2, self.table_view)
+        selection_model = QtGui.QItemSelectionModel(model)
+        self.table_view.setModel(model)
+        self.table_view.setSelectionModel(selection_model)
+
+        model.setHorizontalHeaderItem(0, QtGui.QStandardItem("Zone"))
+        model.setHorizontalHeaderItem(1, QtGui.QStandardItem("Files"))
+
+        self.add_row()
+        
         hbox = QtGui.QHBoxLayout()
         self.ok_button = QtGui.QPushButton("Ok")
         self.cancel_button = QtGui.QPushButton("Cancel")
@@ -25,6 +36,7 @@ class EgvDialog(QtGui.QDialog):
 
 
         vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(self.table_view)
         vbox.addStretch()
         vbox.addLayout(hbox)
         self.setLayout(vbox)
@@ -32,3 +44,11 @@ class EgvDialog(QtGui.QDialog):
     def ok_action(self):
         self.close()
         
+    def add_row(self):
+        """add row to table"""
+        i = 0
+        empty_item = QtGui.QStandardItem("")
+        self.table_view.model().insertRow(i, empty_item)
+
+        #item0 
+        #self.table_view.model().setItem(i, 0, item0)
