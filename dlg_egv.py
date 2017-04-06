b3faf0d2a91793b7c680c17ddbcbd81b4e8e4f1a
Per Gröningsson <per.groningsson@vattenfall.com>
2017-04-06 17:24:41 +0200

    implementing 'insert_table_rows in EgvDialog'

--- dlg_egv.py
+++ dlg_egv.py
@@ -1,3 +1,5 @@
+from pyqt_trace import pyqt_trace as qtrace  # Break point that works with Qt
+import os
 from PyQt4 import QtGui, QtCore
 
 class EgvDialog(QtGui.QDialog):
@@ -10,11 +12,10 @@ class EgvDialog(QtGui.QDialog):
         self.setWindowTitle("EGV settings")
         xpos = self.parent.pos().x() + self.parent.size().width() / 2
         ypos = self.parent.pos().y() + self.parent.size().height() / 2
-        self.setGeometry(QtCore.QRect(0.8*xpos, 0.9*ypos, 500, 200))
+        self.setGeometry(QtCore.QRect(0.8*xpos, 0.9*ypos, 500, 210))
 
         # Table
         self.table = QtGui.QTableWidget(0, 2)
-        #self.table = QtGui.QTableWidget(1, 2)
 
         self.table.setHorizontalHeaderItem(0, QtGui.QTableWidgetItem("Zone"))
         self.table.setHorizontalHeaderItem(1, QtGui.QTableWidgetItem("File"))
@@ -23,14 +24,19 @@ class EgvDialog(QtGui.QDialog):
         verticalheader.setResizeMode(QtGui.QHeaderView.Fixed)
         verticalheader.setDefaultSectionSize(25)
 
-        # Left menu
+        horizontalheader = self.table.horizontalHeader()
+        horizontalheader.setResizeMode(1, QtGui.QHeaderView.Stretch)
+
+        # Group box
         self.version_cbox = QtGui.QComboBox()
         self.version_list = ["2.3.0", "3.2.1"]
         self.version_cbox.addItems(QtCore.QStringList(self.version_list))
 
         self.reactor_cbox = QtGui.QComboBox()
-        self.reactor_list = ["R1", "F1", "F2", "F3"]
+        self.reactor_list = ["F1", "F2", "F3", "R1"]
         self.reactor_cbox.addItems(QtCore.QStringList(self.reactor_list))
+
+        # EGV fue: ATRIUM10B, ATRIUM10XM, SVEA96OPTIMA3
 
         flo = QtGui.QFormLayout()
         flo.addRow("Version:", self.version_cbox)
@@ -46,8 +52,10 @@ class EgvDialog(QtGui.QDialog):
         grid.addWidget(gbox, 0, 0)
         grid.addWidget(self.table, 0, 1)
 
-        self.set_table_rows()
-        
+        self.insert_table_rows()
+        self.table.resizeColumnsToContents()
+        #self.table.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+
         hbox = QtGui.QHBoxLayout()
         self.ok_button = QtGui.QPushButton("Ok")
         self.cancel_button = QtGui.QPushButton("Cancel")
@@ -67,17 +75,54 @@ class EgvDialog(QtGui.QDialog):
 
     def ok_action(self):
         self.close()
-        
-    def set_table_rows(self):
-        """set table rows"""
-        
-        for i in range(3):
-            self.table.insertRow(i)
-            zone_cbox = QtGui.QComboBox()
-            zone_cbox.addItem("None")
-            zone_cbox.addItem("Lower end")
-            zone_cbox.addItem("Lower active")
-            zone_cbox.addItem("Upper active")
-            zone_cbox.addItem("Upper end")
-            self.table.setCellWidget(i, 0, zone_cbox)
+        params  = self.parent.params
+        params.egv_version = str(self.version_cbox.currentText())
+        params.egv_reactor = str(self.reactor_cbox.currentText())
+        nrows = self.table.rowCount()
+        params.egv_zones = []
+        params.egv_files = []
+        for i in range(nrows):
+            zone_cbox = self.table.cellWidget(i, 0)
+            if str(zone_cbox.currentText()) == "None":
+                params.egv_zones.append(None)
+            else:
+                params.egv_zones.append(str(zone_cbox.currentText()))
+            item = self.table.item(i, 1)
+            params.egv_files.append(str(item.text()))
+        params.egv_zones.reverse()
+        params.egv_files.reverse()
 
+    def insert_table_rows(self):
+        """Fill table rows in reverse order"""
+        
+        if hasattr(self.parent, "bunlist"):
+            bundle = self.parent.bunlist[0]
+            caxfiles = bundle.data.caxfiles[::-1]
+            nrows = len(caxfiles)
+            for i in range(nrows):
+                self.table.insertRow(i)
+                vheader = QtGui.QTableWidgetItem(str(nrows - i))
+                self.table.setVerticalHeaderItem(i, vheader)
+                zone_cbox = QtGui.QComboBox()
+                zone_cbox.addItem("None")
+                zone_cbox.addItem("Lower end")
+                zone_cbox.addItem("Lower active")
+                zone_cbox.addItem("Upper active")
+                zone_cbox.addItem("Upper end")
+
+                if hasattr(self.parent.params, "egv_zones"):
+                    zone_list = [str(zone_cbox.itemText(j))
+                                 for j in range(zone_cbox.count())]
+                    if self.parent.params.egv_zones[i]:
+                        zone = self.parent.params.egv_zones[i]
+                    else:
+                        zone = "None"
+                    j = zone_list.index(zone)
+                    print j
+                    #j = 1
+                    zone_cbox.setCurrentIndex(j)
+
+                self.table.setCellWidget(i, 0, zone_cbox)
+                fname = caxfiles[i].split(os.sep)[-1]
+                item = QtGui.QTableWidgetItem(fname)
+                self.table.setItem(i, 1, item)
