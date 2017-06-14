658ae5caf93796a0fd98c7aa6d023e6aa9219ae7
Per Gröningsson <per.groningsson@vattenfall.com>
2017-06-14 14:15:31 +0200

    added all actions to method create_menu

--- ui.py
+++ ui.py
@@ -24,6 +24,7 @@ class Ui_MainWindow(object):
 
         parent.menu_actions = []
 
+        # --- File menu ---
         parent.file_menu = parent.menuBar().addMenu("&File")
 
         new_icon = "new-icon_32x32"
@@ -31,6 +32,7 @@ class Ui_MainWindow(object):
         tip = "Create new bundle"
         args = (text, new_icon, tip, parent.open_bundle_dlg)
         new_action = self.__create_action(*args)
+        parent.file_menu.addAction(new_action)
         parent.menu_actions.append(new_action)
 
         file_icon = "open-file-icon_32x32"
@@ -38,6 +40,7 @@ class Ui_MainWindow(object):
         tip = "Open file"
         args = (text, file_icon, tip, parent.openFile)
         file_action = self.__create_action(*args)
+        parent.file_menu.addAction(file_action)
         parent.menu_actions.append(file_action)
 
         save_icon = "save-icon_32x32"
@@ -45,10 +48,233 @@ class Ui_MainWindow(object):
         tip = "Save data to file"
         args = (text, save_icon, tip, parent.saveData)
         save_action = self.__create_action(*args)
+        parent.file_menu.addAction(save_action)
         parent.menu_actions.append(save_action)
 
-        for action in parent.menu_actions:
-            parent.file_menu.addAction(action)
+        delete_icon = "delete-icon_32x32"
+        text = "&Clear project..."
+        tip = "Clear current project"
+        args = (text, delete_icon, tip, parent.clear_project)
+        clear_action = self.__create_action(*args)
+        parent.file_menu.addAction(clear_action)
+        parent.menu_actions.append(clear_action)
+
+        export_icon = "export-icon_32x32"
+        text = "&Export to ascii..."
+        tip = "Export data to file"
+        args = (text, export_icon, tip, parent.export_to_ascii)
+        save_file_action = self.__create_action(*args)
+        parent.file_menu.addAction(save_file_action)
+        parent.menu_actions.append(save_file_action)
+
+        export_icon = "export-icon_32x32"
+        text = "&Export Figure..."
+        tip = "Export fuel map to file"
+        args = (text, export_icon, tip, parent.saveFigure)
+        save_figure_action = self.__create_action(*args)
+        parent.file_menu.addAction(save_figure_action)
+        parent.menu_actions.append(save_figure_action)
+
+        parent.file_menu.addSeparator()
+
+        quit_icon = "exit-icon_32x32"
+        text = "&Quit"
+        tip = "Exit application"
+        shortcut = "Ctrl+Q"
+        args = (text, quit_icon, tip, parent.close, False, shortcut)
+        quit_action = self.__create_action(*args)
+        parent.file_menu.addAction(quit_action)
+        parent.menu_actions.append(quit_action)
+
+        #for action in parent.menu_actions:
+        #    if action is None:
+        #        parent.file_menu.addSeparator()
+        #    else:
+        #        parent.file_menu.addAction(action)
+
+        # --- Edit menu ---
+        parent.edit_menu = parent.menuBar().addMenu("&Edit")
+
+        back_icon = "arrow-undo-icon_32x32"
+        text = "Back"
+        tip = "Back to previous"
+        args = (text, back_icon, tip, parent.back_state)
+        back_action = self.__create_action(*args)
+        parent.edit_menu.addAction(back_action)
+        parent.menu_actions.append(back_action)
+
+        forward_icon = "arrow-redo-icon_32x32"
+        text = "Forward"
+        tip = "Forward to next"
+        args = (text, forward_icon, tip, parent.forward_state)
+        forward_action = self.__create_action(*args)
+        parent.edit_menu.addAction(forward_action)
+        parent.menu_actions.append(forward_action)
+
+        parent.edit_menu.addSeparator()
+
+        plus_icon = "add-icon_32x32"
+        text = "Increase enr"
+        tip = "Increase enrichment"
+        shortcut = QtCore.Qt.Key_Plus
+        args = (text, plus_icon, tip, parent.enr_add, False, shortcut)
+        increase_enr_action = self.__create_action(*args)
+        parent.edit_menu.addAction(increase_enr_action)
+        parent.menu_actions.append(increase_enr_action)
+        
+        minus_icon = "remove-icon_32x32"
+        text = "Decrease enr"
+        tip = "Decrease enrichment"
+        shortcut = QtCore.Qt.Key_Minus
+        args = (text, minus_icon, tip, parent.enr_sub, False, shortcut)
+        decrease_enr_action = self.__create_action(*args)
+        parent.edit_menu.addAction(decrease_enr_action)
+        parent.menu_actions.append(decrease_enr_action)
+
+        parent.edit_menu.addSeparator()
+
+        layers_icon = "layers-icon_32x32"
+        text = "Segments..."
+        tip = "Edit segment heights..."
+        args = (text, layers_icon, tip, parent.open_segment_dlg)
+        segment_action = self.__create_action(*args)
+        parent.edit_menu.addAction(segment_action)
+        parent.menu_actions.append(segment_action)
+
+        icon = "table-icon_32x32"
+        text = "Enrichments..."
+        tip = "Edit enrichment levels..."
+        args = (text, icon, tip, parent.open_enrichment_dlg)
+        segment_action = self.__create_action(*args)
+        parent.edit_menu.addAction(segment_action)
+        parent.menu_actions.append(segment_action)
+
+        icon = "flame-red-icon_32x32"
+        text = "Quick calc..."
+        tip = "Quick calculation settings..."
+        args = (text, icon, tip, parent.open_pert_dlg)
+        quickcalc_action = self.__create_action(*args)
+        parent.edit_menu.addAction(quickcalc_action)
+        parent.menu_actions.append(quickcalc_action)
+
+        parent.edit_menu.addSeparator()
+
+        icon = "original-icon_32x32"
+        text = "Replace original..."
+        tip = "Replace original design..."
+        args = (text, icon, tip, parent.replace_original_design)
+        replace_action = self.__create_action(*args)
+        parent.edit_menu.addAction(replace_action)
+        parent.menu_actions.append(replace_action)
+
+        icon = "undo-icon_32x32"
+        text = "Reset..."
+        tip = "Reset all changes..."
+        args = (text, icon, tip, parent.reset_state)
+        reset_action = self.__create_action(*args)
+        parent.edit_menu.addAction(reset_action)
+        parent.menu_actions.append(reset_action)
+
+        icon = "preferences-icon_32x32"
+        text = "Preferences..."
+        tip = text
+        args = (text, icon, tip)
+        preferences_action = self.__create_action(*args)
+        parent.edit_menu.addAction(preferences_action)
+        parent.menu_actions.append(preferences_action)
+
+        # --- Tools menu ---
+        parent.tools_menu = parent.menuBar().addMenu("&Tools")
+
+        icon = "diagram-icon_32x32"
+        text = "Plot..."
+        tip = text
+        args = (text, icon, tip, parent.open_plotwin)
+        plot_action = self.__create_action(*args)
+        parent.tools_menu.addAction(plot_action)
+        parent.menu_actions.append(plot_action)
+        
+        icon = "grid-icon_32x32"
+        text = "CASMO..."
+        tip = text
+        args = (text, icon, tip, parent.open_cas_dlg)
+        casmo_action = self.__create_action(*args)
+        parent.tools_menu.addAction(casmo_action)
+        parent.menu_actions.append(casmo_action)
+
+        icon = "write-icon_32x32"
+        text = "Generate inp files"
+        tip = "Generate CASMO input files..."
+        args = (text, icon, tip, parent.generate_inpfiles)
+        casinp_action = self.__create_action(*args)
+        parent.tools_menu.addAction(casinp_action)
+        parent.menu_actions.append(casinp_action)
+
+        icon = "document-icon_32x32"
+        text = "Report..."
+        tip = "Fuel report..."
+        args = (text, icon, tip, parent.open_report_dlg)
+        report_action = self.__create_action(*args)
+        parent.tools_menu.addAction(report_action)
+        parent.menu_actions.append(report_action)
+
+        icon = "binoculars-icon_32x32"
+        text = "Find point..."
+        tip = "Find state point..."
+        args = (text, icon, tip, parent.open_findpoint_dlg)
+        find_action = self.__create_action(*args)
+        parent.tools_menu.addAction(find_action)
+        parent.menu_actions.append(find_action)
+
+        icon = "letter-e-icon_32x32"
+        text = "EGV..."
+        tip = text
+        args = (text, icon, tip, parent.open_egv_dlg)
+        egv_action = self.__create_action(*args)
+        parent.tools_menu.addAction(egv_action)
+        parent.menu_actions.append(egv_action)
+
+        icon = None
+        text = "Show color map"
+        tip = "Show background color map"
+        checkable = True
+        args = (text, icon, tip, parent.toggle_pin_bgcolors, checkable)
+        parent.show_cmap = self.__create_action(*args)
+        parent.tools_menu.addAction(parent.show_cmap)
+        parent.menu_actions.append(parent.show_cmap)
+
+        icon = None
+        text = "Track max pins"
+        tip = "Mark pins with highest value"
+        checkable = True
+        args = (text, icon, tip, parent.toggle_maxpins, checkable)
+        parent.track_maxpin = self.__create_action(*args)
+        parent.tools_menu.addAction(parent.track_maxpin)
+        parent.menu_actions.append(parent.track_maxpin)
+
+        # -- Run menu ---
+        parent.run_menu = parent.menuBar().addMenu("&Run")
+
+        icon = "flame-red-icon_32x32"
+        text = "&Quick calc"
+        tip = "Run quick calc"
+        shortcut = "F9"
+        args = (text, icon, tip, parent.quick_calc, False, shortcut)
+        parent.quickcalc_action = self.__create_action(*args)
+        parent.run_menu.addAction(parent.quickcalc_action)
+        parent.menu_actions.append(parent.quickcalc_action)
+
+        # --- Help menu ---
+        parent.help_menu = parent.menuBar().addMenu("&Help")
+
+        icon = "help-about-icon_32x32"
+        text = "&About"
+        tip = "About"
+        args = (text, icon, tip, parent.on_about)
+        about_action = self.__create_action(*args)
+        parent.help_menu.addAction(about_action)
+        parent.menu_actions.append(about_action)
+        
 
     def create_toolbar(self):
         parent = self.parent
@@ -161,7 +387,7 @@ class Ui_MainWindow(object):
         if slot is not None:
             action.triggered.connect(slot)
         if shortcut is not None:
-            action.setShortCut(shortcut)
+            action.setShortcut(shortcut)
         if checkable:
             action.setCheckable(True)
 
