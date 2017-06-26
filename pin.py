3d8b08ed15a652b9519102d15d054f63999dae50
Per Gröningsson <per.groningsson@vattenfall.com>
2017-06-26 16:17:26 +0200

    code cleanup in pin.py

--- pin.py
+++ pin.py
@@ -10,7 +10,6 @@ except:
     pass
 
 
-#class cpin(object):
 class FuePin(object):
     def __init__(self, axes):
         self.axes = axes
@@ -38,14 +37,13 @@ class FuePin(object):
         self.rectangle.set_linewidth(0.0)
         
     def set_text(self, string='', fsize=8):
-        # if hasattr(self,'text'):
-        #    self.text.remove()
         self.text = self.axes.text(self.x, self.y, string, ha='center',
                                    va='center', fontsize=fsize)
 
     def is_clicked(self, xc, yc):
+        """Check if click is within pin radius"""
+
         r2 = (xc - self.x)**2 + (yc - self.y)**2
-        # Mouse click is within pin radius ?
         if r2 < self.circle.get_radius()**2:
             return True
         else:
@@ -80,70 +78,3 @@ class FuePin(object):
         self.maxpin_patch.set_fill(False)
         self.maxpin_patch.set_linewidth(3.0)
         self.axes.add_patch(self.maxpin_patch)
-
-    #def set_enrpin_patch(self, edge_color=(0.4, 0.4, 0.4)):
-        
-
-class EnrDialog(QtGui.QDialog):
-    def __init__(self, parent, mode="edit"):
-        # QDialog.__init__(self, parent)
-        QtGui.QDialog.__init__(self)
-        self.parent = parent
-        self.mode = mode
-
-        # self.setWindowTitle("Window title")
-        # set x-pos relative to cursor position
-        # xpos = QCursor.pos().x() - 250
-        # set dialog pos relative to main window
-        xpos = parent.pos().x() + parent.size().width() / 2
-        ypos = parent.pos().y() + parent.size().height() / 2
-        self.setGeometry(QtCore.QRect(xpos, ypos, 150, 120))
-
-        case_num = int(parent.ui.case_cbox.currentIndex())
-        ipin = parent.pinselection_index
-        enr = parent.enrpinlist[case_num][ipin].ENR
-        ba = parent.enrpinlist[case_num][ipin].BA
-        ba = 0 if np.isnan(ba) else ba
-        if mode == "edit":
-            self.setWindowTitle("Edit enrichment")
-        elif mode == "add":
-            self.setWindowTitle("Add enrichment")
-        self.enr_text = QtGui.QLineEdit("%.2f" % enr)
-        dens = parent.enrpinlist[case_num][ipin].DENS
-        self.dens_text = QtGui.QLineEdit("%.3f" % dens)
-        self.ba_text = QtGui.QLineEdit("%.2f" % ba)
-        validator = QtGui.QDoubleValidator(0, 9.99, 2, self)
-        self.enr_text.setValidator(validator)
-        self.ba_text.setValidator(validator)
-        validator = QtGui.QDoubleValidator(0, 9.99, 3, self)
-        self.dens_text.setValidator(validator)
-
-        flo = QtGui.QFormLayout()
-        flo.addRow("%U-235:", self.enr_text)
-        flo.addRow("Density (g/cm-3):", self.dens_text)
-        flo.addRow("%Gd:", self.ba_text)
-
-        hbox = QtGui.QHBoxLayout()
-        self.ok_button = QtGui.QPushButton("Ok")
-        self.cancel_button = QtGui.QPushButton("Cancel")
-        hbox.addWidget(self.cancel_button)
-        hbox.addWidget(self.ok_button)
-        self.connect(self.cancel_button, QtCore.SIGNAL('clicked()'),
-                     self.close)
-        self.connect(self.ok_button, QtCore.SIGNAL('clicked()'), self.action)
-
-        vbox = QtGui.QVBoxLayout()
-        vbox.addLayout(flo)
-        vbox.addStretch()
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
-
-    def action(self):
-        self.close()
-        self.enr = self.enr_text.text().toDouble()[0]
-        self.dens = self.dens_text.text().toDouble()[0]
-        self.ba = self.ba_text.text().toDouble()[0]
-        if self.mode == "edit":
-            self.parent.enrpin_edit_callback()
-        elif self.mode == "add":
-            self.parent.enrpin_add_callback()
